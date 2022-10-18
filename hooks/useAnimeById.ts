8b0f2d0b4f479f0f18ec14a337f30962ce5899b8
IssaMDOunejjaR <52113338+IssaMDOunejjaR@users.noreply.github.com>
2022-10-18 12:00:16 +0100

    add build in workflow

---
+++ hooks/useAnimeById.ts
@@ -0,0 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAnimesByPopularity } from "../fetchers/media";
+
+export const useAnimeById = (id: number) => {
+	// return useQuery([`anime-`], () => );
+};
